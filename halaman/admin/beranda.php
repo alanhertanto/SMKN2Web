6df4c9f1d5f14be9befa996391b67d4a3ad0287d
Alan Hertanto Guspermana <alanhertantog@gmail.com>
2022-08-25 22:22:52 +0700

    Little Update

---
+++ halaman/admin/beranda.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin SMKN 2 Kuningan</title>
+    <link rel="apple-touch-icon" sizes="57x57" href="../../assets//apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../../assets//apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../../assets//apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../../assets//apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../../assets//apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../../assets//apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../../assets//apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../../assets//apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../../assets//apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="../../assets//android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../assets//favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../../assets//favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../assets//favicon-16x16.png">
+    <link rel="manifest" href="../../assets//manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="../../assets//ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+
+    <!-- Custom fonts for this template-->
+    <link href="../../assets/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="../../assets/admin/css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+               
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    <!-- Content Row -->
+                    <div class="row">
+                        <div class="col-xl-12 col-md-12 mb-12">
+                            <center>
+                                <h1>Selamat Datang Di Halaman Guru SMKN 2 Kuningan</h1>
+                                <br>
+                                <img src="admin.png" width="400" height="400">
+                            </center>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+   
+
+    <!-- Logout Modal-->
+        
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="../../assets/admin/vendor/jquery/jquery.min.js"></script>
+    <script src="../../assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../../assets/admin/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../../assets/admin/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="../../assets/admin/vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="../../assets/admin/js/demo/chart-area-demo.js"></script>
+    <script src="../../assets/admin/js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
